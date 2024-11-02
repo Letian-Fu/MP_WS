52f016e92ba65d9d976c795a2ed64dc66243a9f5
letianfu <letianfu@gmail.com>
2024-11-02 15:52:49 +0800

    Initialization

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/roboert/MP_WS/src
+# Build directory: /home/roboert/MP_WS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/roboert/MP_WS/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/roboert/ws_moveit/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/local/share:/usr/local/lib:/usr/local/cuda/lib64'
+export PATH='/opt/ros/noetic/bin:/home/roboert/anaconda3/bin:/home/roboert/anaconda3/condabin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda/bin'
+export PWD='/home/roboert/MP_WS/build'
+export PYTHONPATH='/home/roboert/ws_moveit/devel/lib/python3/dist-packages:/opt/ros/noetic/lib/python3/dist-packages:/usr/local/cython'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/roboert/MP_WS/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/roboert/MP_WS/src:$ROS_PACKAGE_PATH"
