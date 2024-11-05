52cffa713c0fb75078ade7c39b1b677435669685
letianfu <letianfu@gmail.com>
2024-11-05 20:10:26 +0800

    Update gazebo simulation

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,9 +5,8 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("cr5_dh_moveit_config")
+subdirs("cr5_moveit_config")
 subdirs("gpmp2_planner_py")
 subdirs("cr5_gazebo")
 subdirs("cr5_description")
-subdirs("cr5_moveit_config")
 subdirs("cr5_planner")
